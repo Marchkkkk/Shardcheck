eccf5974077b3d55deaaead4e491e23b6eea558e
Mark Popov <104899636+Marchkkkk@users.noreply.github.com>
2024-08-17 04:40:06 +0200

    Update check_shardeum_status.sh

--- check_shardeum_status.sh
+++ check_shardeum_status.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Отримання директорії, в якій розміщено цей скрипт
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
 # Завантаження конфігурації
-source "$SCRIPT_DIR/config.sh"
-
-# Файл для зберігання логу статусу ноди
-STATUS_LOG="$SCRIPT_DIR/status_output.txt"
+source "$HOME/shardeum_tg_checker/config.sh"
 
 # Функція для перевірки статусу ноди
 check_node_status() {
     # Перехід до директорії і виконання shell.sh
-    if ! cd ~/.shardeum || ! ./shell.sh; then
-        echo "Не вдалося перейти до директорії або виконати shell.sh" >> "$STATUS_LOG"
+    if ! cd ~/.shardeum 2>>"$HOME/shardeum_tg_checker/status_output.txt"; then
+        echo "Не вдалося перейти до директорії ~/.shardeum" >> "$HOME/shardeum_tg_checker/status_output.txt"
+        return 1
+    fi
+    sleep 5
+
+    if ! ./shell.sh 2>>"$HOME/shardeum_tg_checker/status_output.txt"; then
+        echo "Не вдалося виконати shell.sh" >> "$HOME/shardeum_tg_checker/status_output.txt"
         return 1
     fi
 
@@ -22,16 +22,16 @@ check_node_status() {
 
     # Отримання статусу ноди та запис всього вихідного результату у файл для відлагодження
     full_output=$(operator-cli status 2>&1)
-    echo "$full_output" > "$STATUS_LOG"
+    echo "$full_output" > "$HOME/shardeum_tg_checker/status_output.txt"
 
     # Виведення відлагоджувальної інформації в консоль
     echo "Отриманий вихідний результат:"
-    echo "$full_output"
+    echo "$full_output" >> "$HOME/shardeum_tg_checker/status_output.txt"
 
     # Спроба знайти статус в отриманому результаті
     status=$(echo "$full_output" | grep "state:" | awk '{print $2}')
     if [ -z "$status" ]; then
-        echo "Не вдалося отримати статус ноди" >> "$STATUS_LOG"
+        echo "Не вдалося отримати статус ноди" >> "$HOME/shardeum_tg_checker/status_output.txt"
         return 1
     fi
 
@@ -46,7 +46,7 @@ if ! check_node_status; then
     # Надсилання повідомлення в Telegram
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d text="$message"
 
-    echo "Повідомлення про помилку надіслано в Telegram." >> "$STATUS_LOG"
+    echo "Повідомлення про помилку надіслано в Telegram."
     exit 1
 fi
 
@@ -56,4 +56,4 @@ message="Сервер №: $SERVER_NAME%0AIP: $(hostname -I | awk '{print $1}')%
 # Надсилання повідомлення в Telegram
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d text="$message"
 
-echo "Повідомлення надіслано в Telegram." >> "$STATUS_LOG"
+echo "Повідомлення надіслано в Telegram."
