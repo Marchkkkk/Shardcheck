0ed67949f76f973e49821fe6b772f030cb9c9e15
Mark Popov <104899636+Marchkkkk@users.noreply.github.com>
2024-08-17 03:40:10 +0200

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remove the cron job
-crontab -l | grep -v "/usr/local/shardeum_status/check_shardeum_status.sh" | crontab -
-
-# Remove the script directory
-rm -rf /usr/local/shardeum_status
-
-echo "Script and all its components have been removed." 
